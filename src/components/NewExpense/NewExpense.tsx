fbbad11fc5ea81178ff71643b7768ed030f4e582
Németh Andris <n.andris939@gmail.com>
2023-04-12 09:44:29 +0200

    Add NewExpense.tsx and css

---
+++ src/components/NewExpense/NewExpense.tsx
@@ -0,0 +1,41 @@
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+import { Item } from "../../interfaces/Item";
+import { useState } from "react";
+
+function NewExpense(props: any) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function saveExpenseDataHandler(enteredExpenseData: Item) {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  }
+
+  function startEditingHandler() {
+    setIsEditing(true);
+  }
+
+  function stopEditingHandler() {
+    setIsEditing(false);
+  }
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
+    </div>
+  );
+}
+
+export default NewExpense;
